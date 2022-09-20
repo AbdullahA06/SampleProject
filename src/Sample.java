63b51ee1af9070596561d3619204adb1746782af
AbdullahA06 <aabdullaev0283@bths.edu>
2022-09-20 11:43:32 -0400

    Added a print statement including my favorite movie

--- src/Sample.java
+++ src/Sample.java
@@ -1,5 +1,6 @@
 public class Sample {
     public static void main(String[] args){
         System.out.print("My name is Abdullah");
+        System.out.print("My favorite movie of all time is Fight Club");
     }
 }
