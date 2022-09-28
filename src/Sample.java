c6ca3a38147b2ca400b5d1ee78b7361e99b0a192
AbdullahA06 <aabdullaev0283@bths.edu>
2022-09-27 23:27:49 -0400

    Added print command with my favorite food

--- src/Sample.java
+++ src/Sample.java
@@ -2,5 +2,6 @@ public class Sample {
     public static void main(String[] args){
         System.out.print("My name is Abdullah");
         System.out.print("My favorite movie of all time is Fight Club");
+        System.out.print("My favorite food is pasta because of how versatile it is.");
     }
 }
